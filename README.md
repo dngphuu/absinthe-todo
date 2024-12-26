3a652545ca36efc2ee0fc43677bb7261dd13f883
dngphuu <dngphuu@gmail.com>
2024-12-26 20:36:15 +0700

    Config files and main file

---
+++ README.md
@@ -0,0 +1,5 @@
+# REQUIREMENTS
+
+- Python
+- NodeJS
+- TailwindCSS
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./templates/**/*.html", // Thêm dòng này để quét tất cả file HTML trong thư mục templates
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
