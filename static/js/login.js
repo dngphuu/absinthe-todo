b9fe587d74cdbf1d48a52da7b70f51f4afeed4d4
dngphuu <dngphuu@gmail.com>
2024-12-30 14:19:26 +0700

    Config login button

---
+++ static/js/login.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const loginBtn = document.querySelector(".click-fade-opacity-button");
+  loginBtn.addEventListener("click", function () {
+    loginBtn.classList.add("");
+  });
+});
