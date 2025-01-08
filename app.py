0db72a9e63b4d907b6c148baa54d82952a68716c
dngphuu <dngphuu@gmail.com>
2025-01-08 10:34:07 +0700

    - Add login feature (demo)

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, session, url_for
 
 app = Flask(__name__, template_folder="templates")
+app.secret_key = "secret_key"
 
-
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        session['isAuth'] = True
+        return redirect(url_for("index"))
     return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.pop('isAuth', None)
+    return redirect(url_for("login"))
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if session.get('isAuth'):
+        return render_template("index.html")
+    else:
+        return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
