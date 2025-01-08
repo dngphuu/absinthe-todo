0f0c5d4bd1334945ef2a3986a4b2873fc0db351c
dngphuu <dngphuu@gmail.com>
2025-01-08 10:08:53 +0700

    - Delete unused files

--- static/js/login.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const loginBtn = document.querySelector(".click-fade-opacity-button");
-  loginBtn.addEventListener("click", function () {
-    loginBtn.classList.add("");
-  });
-});
