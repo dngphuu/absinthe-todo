4a307275c8e08c711c6003411f0d07840f50f1d9
dngphuu <dngphuu@gmail.com>
2025-01-09 15:23:48 +0700

    Add animation to log out menu

--- static/js/app.js
+++ static/js/app.js
@@ -5,9 +5,15 @@ document.getElementById("user-info").addEventListener("click", function () {
 
   if (LOGOUT_MENU.classList.contains("hidden")) {
     LOGOUT_MENU.classList.remove("hidden");
-    LOGOUT_MENU.classList.add("block");
+    setTimeout(() => {
+      LOGOUT_MENU.classList.remove("-translate-y-4", "opacity-0");
+      LOGOUT_MENU.classList.add("translate-y-0", "opacity-100");
+    }, 10);
   } else {
-    LOGOUT_MENU.classList.remove("block");
-    LOGOUT_MENU.classList.add("hidden");
+    LOGOUT_MENU.classList.add("-translate-y-4", "opacity-0");
+    LOGOUT_MENU.classList.remove("translate-y-0", "opacity-100");
+    setTimeout(() => {
+      LOGOUT_MENU.classList.add("hidden");
+    }, 300);
   }
 });
