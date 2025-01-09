94febaacce6ed76334ceca1158c80aafb3425fc1
dngphuu <dngphuu@gmail.com>
2025-01-09 14:54:11 +0700

    Responsive design for user avatar - Delete unused file: style.css

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,13 @@
 // SHOW LOGOUT MENU
 
-document.getElementById("user-avatar").addEventListener("click", function () {
+document.getElementById("user-info").addEventListener("click", function () {
   const LOGOUT_MENU = document.getElementById("logout-menu");
 
   if (LOGOUT_MENU.classList.contains("hidden")) {
     LOGOUT_MENU.classList.remove("hidden");
-    LOGOUT_MENU.classList.add("show");
+    LOGOUT_MENU.classList.add("block");
   } else {
-    LOGOUT_MENU.classList.remove("show");
+    LOGOUT_MENU.classList.remove("block");
     LOGOUT_MENU.classList.add("hidden");
   }
 });
